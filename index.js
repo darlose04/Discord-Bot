428422f78f68409e9696783ffbf385b56e7a82b8
Zach Smith <zsmith126028@gmail.com>
2020-03-23 16:01:59 -0500

    playing around with different options for messages

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 require("dotenv").config();
 
+// const PREFIX = "!";
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on("message", msg => {
   if (msg.content === "ping") {
-    msg.channel.send("pong");
+    // msg.channel.send("pong");
+    // msg.reply("pong");
+
+    console.log(msg.author.username);
+  }
+
+  if (msg.author.username === "mwl4h9") {
+    msg.reply("All hail Commisioner Luhn!!!");
+    msg.reply("All hail Commisioner Luhn!!!");
+    msg.reply("All hail Commisioner Luhn!!!");
   }
 });
+
+client.on("message", msg => {});
 
 let token = process.env.TOKEN;
 
