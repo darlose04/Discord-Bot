d77068f306b3f8d0160a9823a27b8116463939c0
Zach Smith <zsmith126028@gmail.com>
2020-03-23 16:12:10 -0500

    Update index.js
    remove commented code

--- index.js
+++ index.js
@@ -9,13 +9,6 @@ client.on("ready", () => {
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    // msg.channel.send("pong");
-    // msg.reply("pong");
-
-    console.log(msg.author.username);
-  }
-
   if (msg.author.username === "mwl4h9") {
     msg.channel.send("All hail Commissioner Luhn!!!");
     msg.channel.send("All hail Commissioner Luhn!!!");
