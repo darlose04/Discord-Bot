508582a929ea83ed3965b7f4a630da3a50749abe
Zach Smith <zsmith126028@gmail.com>
2020-03-26 13:08:53 -0500

    add player type to the entry in order to specify batting, starting, or relieving information

--- index.js
+++ index.js
@@ -10,10 +10,39 @@ client.on("ready", () => {
 });
 
 client.on("message", msg => {
-  let members = [];
-  client.users.cache.map(user => {
-    members.push(user.username);
-  });
+  // let members = [];
+  // client.users.cache.map(user => {
+  //   members.push(user.username);
+  // });
+
+  // get batting information
+  const getBatterInfo = async (name, season) => {
+    try {
+      const url = "https://www.fgbaseballapi.com/api/";
+
+      let battingStats = await axios.get(`${url}playerbatting/players/${name}`);
+
+      if (msg.content[0] === "!" && battingStats.data.length === 0) {
+        msg.reply("Unable to retrieve batting data");
+      }
+
+      let stats = battingStats.data.filter(item => item.season === season)[0];
+
+      let statsArr = [];
+
+      // console.log(stats);
+
+      Object.keys(stats).forEach(function(key) {
+        statsArr.push(`${key}: ${stats[key]}`);
+      });
+      // console.log(statsArr);
+
+      msg.channel.send(statsArr);
+    } catch (error) {
+      console.log("This is the error: ");
+      console.log(error);
+    }
+  };
 
   // get relief pitching information
   const getRelieverInfo = async (name, season) => {
@@ -77,48 +106,24 @@ client.on("message", msg => {
     }
   };
 
-  // get batting information
-  const getBatterInfo = async (name, season) => {
-    try {
-      const url = "https://www.fgbaseballapi.com/api/";
-
-      let battingStats = await axios.get(`${url}playerbatting/players/${name}`);
-
-      if (msg.content[0] === "!" && battingStats.data.length === 0) {
-        msg.reply("Unable to retrieve batting data");
-      }
-
-      let stats = battingStats.data.filter(item => item.season === season)[0];
-
-      let statsArr = [];
-
-      // console.log(stats);
-
-      Object.keys(stats).forEach(function(key) {
-        statsArr.push(`${key}: ${stats[key]}`);
-      });
-      // console.log(statsArr);
-
-      msg.reply(statsArr);
-    } catch (error) {
-      console.log("This is the error: ");
-      console.log(error);
-    }
-  };
-
   if (msg.content[0] === "!") {
     let args = msg.content.substring(prefix.length).split(" ");
 
     let year = parseInt(args.pop());
+    let type = args.shift();
     let player = args.join(" ");
 
-    // msg.reply(
-    //   "Please respond with the type of information you want: Batting, Starting, or Relieving."
-    // );
-
-    // getBatterInfo(player, year);
-    // getStarterInfo(player, year);
-    // getRelieverInfo(player, year);
+    if (type === "Batting") {
+      getBatterInfo(player, year);
+    } else if (type === "Starting") {
+      getStarterInfo(player, year);
+    } else if (type === "Relieving") {
+      getRelieverInfo(player, year);
+    } else {
+      msg.reply(
+        "Invalid information. Please enter in this format: !Type Name Year. Ex: !Batting Mike Trout 2019. Types are Batting, Starting, Relieving."
+      );
+    }
   }
 });
 
