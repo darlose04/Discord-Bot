0fac1918be949fa9f6cedf6f714dfecdd60761ae
Zach Smith <zsmith126028@gmail.com>
2020-03-26 09:46:25 -0500

    set player value to be string without exclamation point. Make axios call to api

--- index.js
+++ index.js
@@ -17,21 +17,15 @@ client.on("message", msg => {
 
   let args = msg.content.substring(prefix.length).split("!");
 
-  if (msg.content[0] === "!") {
-    msg.reply("Is this ");
-  }
-
-  console.log(args[0]);
+  let player = args[0];
 
   const getPlayerInfo = async name => {
-    let info = await axios.get();
+    const url = "https://www.fgbaseballapi.com/api/";
+    let battingStats = await axios.get(`${url}playerbatting/players/${name}`);
+    console.log(battingStats.data);
   };
 
-  // if (msg.author.username === "mwl4h9") {
-  //   msg.channel.send("All hail Commissioner Luhn!!!");
-  //   msg.channel.send("All hail Commissioner Luhn!!!");
-  //   msg.channel.send("All hail Commissioner Luhn!!!");
-  // }
+  getPlayerInfo(player);
 });
 
 client.on("message", msg => {});
